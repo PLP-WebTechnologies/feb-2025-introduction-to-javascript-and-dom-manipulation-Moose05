776b4e84db19327de58cfa5f0b4be6674f0a925b
Moose05 <69054872+Moose05@users.noreply.github.com>
2025-05-17 14:51:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function changeText() {
+  const textEl = document.getElementById('dynamicText');
+  textEl.textContent = "Text Changed Dynamically!";
+}
 
+function toggleStyle() {
+  const textEl = document.getElementById('dynamicText');
+  textEl.classList.toggle('highlight');
+}
+
+function addOrRemoveBox() {
+  const container = document.getElementById('boxContainer');
+  const existingBox = document.getElementById('box');
+
+  if (existingBox) {
+    container.removeChild(existingBox);
+  } else {
+    const box = document.createElement('div');
+    box.id = 'box';
+    box.className = 'box';
+    container.appendChild(box);
+  }
+}
